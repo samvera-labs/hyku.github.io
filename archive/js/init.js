61fa1c0b7721b9848c02ba82b989fa8002c5efd5
Rob Kaufman <rob@notch8.com>
2019-07-24 14:26:45 -0700

    old vs archive

--- archive/js/init.js
+++ archive/js/init.js
@@ -13,7 +13,7 @@
 			// Global.
 				global: {
 					range: '*',
-					href: '/old/css/style.css',
+					href: '/archive/css/style.css',
 					containers: 1400,
 					grid: {
 						gutters: {
@@ -26,14 +26,14 @@
 			// XLarge.
 				xlarge: {
 					range: '-1680',
-					href: '/old/css/style-xlarge.css',
+					href: '/archive/css/style-xlarge.css',
 					containers: 1200
 				},
 
 			// Large.
 				large: {
 					range: '-1280',
-					href: '/old/css/style-large.css',
+					href: '/archive/css/style-large.css',
 					containers: 960,
 					grid: {
 						gutters: {
@@ -48,7 +48,7 @@
 			// Medium.
 				medium: {
 					range: '-980',
-					href: '/old/css/style-medium.css',
+					href: '/archive/css/style-medium.css',
 					containers: '90%',
 					grid: {
 						collapse: 1
@@ -58,7 +58,7 @@
 			// Small.
 				small: {
 					range: '-736',
-					href: '/old/css/style-small.css',
+					href: '/archive/css/style-small.css',
 					containers: '90%',
 					grid: {
 						gutters: {
@@ -70,7 +70,7 @@
 			// XSmall.
 				xsmall: {
 					range: '-480',
-					href: '/old/css/style-xsmall.css',
+					href: '/archive/css/style-xsmall.css',
 					grid: {
 						collapse: 2
 					}
